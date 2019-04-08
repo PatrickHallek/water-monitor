7e7eaaa52bfaa3a1132d8384fd60b6fed02eaa72
Patrick <patrick.hallek@exxeta.com>
2019-04-08 22:04:30 +0200

    Add Wifi search and credential form field

--- src/app/network/tab1.page.ts
+++ src/app/network/tab1.page.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WifiService } from 'src/service/wifi.service';
+import { Subscription } from 'rxjs';
+import { AnimationService } from 'src/service/animation.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
-  constructor(private wifiService: WifiService) { }
+export class Tab1Page implements OnInit {
+  constructor(private wifiService: WifiService,
+    private animationService: AnimationService) { }
   public ssid;
   public password;
-  public wifi = [];
+  public wifi = [{ SSID: "Finger weg da" }, { SSID: "WLANOTO" }, { SSID: "FRITZBOX", }];
+  public spinner: Boolean = false;
+  public spinnerSub: Subscription;
+
+  ngOnInit() {
+    this.spinnerSub = this.animationService
+      .getSpinnerListener()
+      .subscribe((spinner: Boolean) => {
+        this.spinner = spinner;
+      });
+  }
 
   sendCredentials() {
-    this.ssid = "Finger weg da";
-    this.password = "24680fJhNsdawelobxYA0987AsDfGhBFE";
+    this.animationService.setSpinner(true);
+    console.log("Password: " + this.password + " SSID: " + this.ssid);
+    // this.ssid = "Finger weg da";
+    // this.password = "24680fJhNsdawelobxYA0987AsDfGhBFE";
     this.wifiService.sendCredentials(this.ssid, this.password)
   }
 
@@ -23,6 +38,14 @@ export class Tab1Page {
     setTimeout(() => {
       this.wifi = this.wifiService.networks;
     }, 500)
+  }
+
+  getSSID(ssid) {
+    this.ssid = ssid;
+  }
+
+  unsetSSID() {
+    this.ssid = undefined;
   }
 
 }
